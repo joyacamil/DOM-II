084a73aa977cb19e096912b36b93935738ad9dd0
joyacamil <deshaunarainer@gmail.com>
2019-09-06 22:11:31 -0400

    added mouseover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // Your code goes here
+
+// `mouseover`
+
+const busImg = document.querySelector('.home .intro img');
+busImg.addEventListener('mouseover', (event) => {
+  alert('Welcome to Fun Bus! Where travel is no fuss.');
+});
+
+
+// `keydown`
+// `wheel`
+// `drag / drop`
+// `load`
+// `focus`
+// `resize`
+// `scroll`
+// `select`
+// `dblclick`
