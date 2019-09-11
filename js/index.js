a63fa0d9fe4400cab5d9f21632c6b20749227e98
joyacamil <deshaunarainer@gmail.com>
2019-09-10 22:05:26 -0400

    offline resize scroll mouseover and dblclick created

--- js/index.js
+++ js/index.js
@@ -1,19 +1,41 @@
 // Your code goes here
 
-// `mouseover`
-
+const logo = document.querySelector('.logo-heading');
 const busImg = document.querySelector('.home .intro img');
-busImg.addEventListener('mouseover', (event) => {
-  alert('Welcome to Fun Bus! Where travel is no fuss.');
+const navBar = document.querySelector('.nav');
+const navItems = document.querySelectorAll('.nav-link');
+const container = document.querySelector('.container')
+const header = document.querySelector('.intro h2');
+const paragraphs = document.querySelectorAll('p');
+const destinationH = document.querySelectorAll('.destination h4')
+const buttons = document.querySelectorAll('.btn');
+const footer = document.querySelector('.footer');
+
+//offline
+window.addEventListener('offline', (event) => {
+  alert("Looks like you lost network connection. Please reconnect to proceed. HURRY!");
 });
 
+// `resize`
+window.addEventListener('resize', () => console.log('Resized.'));
+
+// `scroll`
+window.addEventListener('scroll',(e) => console.log(e));
+
+// `mouseover`
+busImg.addEventListener('mouseover', () => busImg.style.margin = '60px 0 60px 0');
 
 // `keydown`
+
+
 // `wheel`
-// `drag / drop`
+
 // `load`
 // `focus`
-// `resize`
-// `scroll`
+
+
 // `select`
+
 // `dblclick`
+footer.addEventListener('dblclick',() => footer.style.backgroundColor = 'green');
+
